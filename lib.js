5a3f6b3dc152f9398094a6056a5a8c25b5de5b0d
Ryan Tharp <neuro@interx.net>
2020-10-14 04:51:21 +0000

    runStorageRPCTest handle exceptions from httpGet, libc note for CI

--- lib.js
+++ lib.js
@@ -101,6 +101,9 @@ Cant detect blockchain version Error: Command failed: /opt/loki-launcher/bin/lok
       if (e.signal === 'SIGILL') {
         console.error("Cannot detect blockchain version. Your current lokid binary does not support your CPU")
       } else {
+        if (e.signal === null && e.status === 1 && e.error === null) {
+          // LIBC error
+        }
         console.error("Cannot detect blockchain version", e)
         //console.error('Cant detect blockchain version', e.stdout.toString())
       }
@@ -591,7 +594,22 @@ async function runStorageRPCTest(lokinet, config, cb) {
   }, 5000)
   var oldTLSValue = process.env["NODE_TLS_REJECT_UNAUTHORIZED"]
   process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0' // turn it off for now
-  const data =  await lokinet.httpGet(url)
+  let data
+  try {
+    data =  await lokinet.httpGet(url)
+  } catch(e) {
+    if (e !== undefined) {
+      if (e.code === 'ECONNREFUSED') {
+        // simply not listening/running (yet...?)
+      } else {
+        // most e are already displayed in httpGet
+        //console.error('runStorageRPCTest httpGet failure', e)
+      }
+    } else {
+      // a reject...
+      // shutdown or 404/403
+    }
+  }
   process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = oldTLSValue
   clearTimeout(storage_rpc_timer)
   if (responded) return
